168a93e31c2fa6535fcce792d1757994fb2740a4
Greg Chapple <gregchapple1@gmail.com>
2014-07-17 17:17:26 +0100

    Messing with event channels

--- src/rdit.rs
+++ src/rdit.rs
@@ -1,19 +1,49 @@
 extern crate rustbox;
+use std::comm::{Receiver};
+
+struct Editor {
+    events: Receiver<rustbox::Event>,
+}
+
+enum Response {
+    Continue,
+    Quit,
+}
+
+impl Editor {
+    pub fn handle_events(&self) -> Response {
+        let evt = self.events.recv();
+        match evt {
+            rustbox::KeyEvent(_, _, ch) => {
+                match std::char::from_u32(ch) {
+                    Some('q') => Quit,
+                    _ => Continue,
+                }
+            },
+            _ => Continue,
+        }
+    }
+
+    pub fn start(&self) -> Response {
+        let resp = self.handle_events();
+        resp
+    }
+}
 
 fn main() {
     rustbox::init();
 
-    loop {
-        match rustbox::poll_event() {
-            rustbox::KeyEvent(_, _, ch) => {
-                match std::char::from_u32(ch) {
-                    Some('q') => { break; },
-                    _ => {}
-                }
-            },
-            _ => { }
+    let(events, receiver) = channel();
+    let editor = Editor {events: receiver};
+
+    spawn(proc() {
+        loop {
+            events.send(rustbox::poll_event());
+            //break;
         }
-    }
+    });
+
+    editor.start();
 
     rustbox::shutdown();
 }
