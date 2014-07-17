028a2572074809dfff3c082d91b466188cad5b97
Greg Chapple <gregchapple1@gmail.com>
2014-07-17 17:40:08 +0100

    Change events loop

--- src/rdit.rs
+++ src/rdit.rs
@@ -11,22 +11,29 @@ enum Response {
 }
 
 impl Editor {
-    pub fn handle_events(&self) -> Response {
-        let evt = self.events.recv();
-        match evt {
-            rustbox::KeyEvent(_, _, ch) => {
-                match std::char::from_u32(ch) {
-                    Some('q') => Quit,
-                    _ => Continue,
-                }
-            },
+    pub fn handle_key_event(&self, ch: u32) -> Response {
+        match std::char::from_u32(ch) {
+            Some('q') => Quit,
             _ => Continue,
         }
     }
 
-    pub fn start(&self) -> Response {
-        let resp = self.handle_events();
-        resp
+    pub fn start(&self) -> bool {
+        loop {
+            let status = match self.events.recv() {
+                rustbox::KeyEvent(_, _, ch) => {
+                    let status = self.handle_key_event(ch);
+                    match status {
+                        Quit => { false },
+                        _ => { true },
+                    }
+                },
+                _ => { true }
+            };
+            if status == false {
+                return false;
+            }
+        }
     }
 }
 
