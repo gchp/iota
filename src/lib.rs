dc3cc1bc1a67e931f8cc9ee6403bdb869a5d4b5b
Greg Chapple <gregchapple1@gmail.com>
2014-11-28 16:56:08 +0000

    restructure file

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
 pub use editor::Editor;
 
-pub enum Response {
-    Continue,
-    Quit,
-}
-
 mod utils;
 mod buffer;
 mod editor;
 mod cursor;
 mod keyboard;
 mod view;
+
+pub enum Response {
+    Continue,
+    Quit,
+}
