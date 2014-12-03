7618b2cbc54dd1bcbed132aac59e0c63569dec5b
Greg Chapple <gregchapple1@gmail.com>
2014-12-03 12:24:32 +0000

    Added tests for uibuf module

--- src/iota/uibuf.rs
+++ src/iota/uibuf.rs
@@ -4,22 +4,22 @@ extern crate rustbox;
 pub struct UIBuffer {
     width: uint,
     height: uint,
-    cells: Vec<Vec<Cell>>
+    rows: Vec<Vec<Cell>>
 }
 
 impl UIBuffer {
     pub fn new(width: uint, height: uint) -> UIBuffer {
-        let cells = Cell::create_grid(width, height, ' ');
+        let rows = Cell::create_grid(width, height, ' ');
 
         UIBuffer {
             width: width,
             height: height,
-            cells: cells,
+            rows: rows,
         }
     }
 
     pub fn draw_range(&self, start: uint, stop: uint) {
-        let rows = self.cells.slice(start, stop);
+        let rows = self.rows.slice(start, stop);
         for row in rows.iter() {
             for cell in row.iter() {
                 rustbox::print_char(cell.x, cell.y, rustbox::Style::Normal, cell.fg, cell.bg, cell.ch);
@@ -29,17 +29,17 @@ impl UIBuffer {
 
     /// Recreated the entire grid, will cells containing `ch`.
     pub fn fill(&mut self, ch: char) {
-        self.cells = Cell::create_grid(self.width, self.height, ch);
+        self.rows = Cell::create_grid(self.width, self.height, ch);
     }
 
     /// Update the `ch` attribute of an individual cell
-    pub fn update_cell_content(&mut self, x: uint, y: uint, ch: char) {
-        self.cells[y][x].ch = ch
+    pub fn update_cell_content(&mut self, cell_num: uint, row_num: uint, ch: char) {
+        self.rows[row_num][cell_num].ch = ch
     }
 
     /// Update the `ch`, `fg`, and `bg` attributes of an indivudual cell
-    pub fn update_cell(&mut self, x: uint, y: uint, ch: char, fg: rustbox::Color, bg: rustbox::Color) {
-        let cell = &mut self.cells[y][x];
+    pub fn update_cell(&mut self, cell_num: uint, row_num: uint, ch: char, fg: rustbox::Color, bg: rustbox::Color) {
+        let cell = &mut self.rows[row_num][cell_num];
         cell.ch = ch;
         cell.fg = fg;
         cell.bg = bg;
@@ -85,3 +85,63 @@ impl Cell {
     }
 }
 
+
+#[cfg(test)]
+mod tests {
+
+    extern crate rustbox;
+
+    use uibuf::UIBuffer;
+
+    fn setup_uibuf() -> UIBuffer {
+        UIBuffer::new(50, 50)
+    }
+
+    #[test]
+    fn grid_is_created_correctly() {
+        let uibuf = setup_uibuf();
+        assert_eq!(uibuf.height, 50);
+        assert_eq!(uibuf.width, 50);
+        assert_eq!(uibuf.rows.len(), 50);
+        assert_eq!(uibuf.rows[0][5].ch, ' ');
+    }
+
+    #[test]
+    fn fill_updates_contents_of_all_cells() {
+        let mut uibuf = setup_uibuf();
+        uibuf.fill('x');
+
+        // check some random cells
+        assert_eq!(uibuf.rows[20][5].ch, 'x');
+        assert_eq!(uibuf.rows[0][30].ch, 'x');
+    }
+
+    #[test]
+    fn update_cell_content_updates_a_single_cell() {
+        let mut uibuf = setup_uibuf();
+        // update cell 10 in row 32
+        uibuf.update_cell_content(10, 32, 'y');
+
+        assert_eq!(uibuf.rows[32][10].ch, 'y');
+    }
+
+    #[test]
+    fn update_cell_updates_all_attrs_of_cell() {
+        let mut uibuf = setup_uibuf();
+        let cell_num = 10u;
+        let row_num = 0u;
+        let ch = 'q';
+        let fg = rustbox::Color::Red;
+        let bg = rustbox::Color::Blue;
+
+        uibuf.update_cell(cell_num, row_num, ch, fg, bg);
+
+        let cell = &uibuf.rows[row_num][cell_num];
+        assert_eq!(cell.ch, ch);
+
+        // FIXME(greg): this fails cos '==' is not implemented for rustbox::Color.
+        // assert_eq!(cell.fg, fg);
+        // assert_eq!(cell.bg, bg);
+    }
+
+}
