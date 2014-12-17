a8c50ec3f036f2e5a146684a5480017e6c73fcea
Joshua Yanovski <pythonesque@gmail.com>
2014-12-17 04:12:56 -0800

    Remove the extra backtrace stuff.
    With the recent changes to RustBox, main looks extremely straightforward
    now, while still being totally safe.  The previous solution was not
    quite correct since it would do things like duplicate error messages if
    you redirected stderr, and only captured stderr output on panic!.  The
    new solution lets you tell RustBox to automatically redirect stderr to
    an internal pipe, which will get printed at the end of the program.
    This allows us to avoid hooking into the panic system explicitly and is
    also much easier for users to use.  It also lets us get rid of the final
    unsafe block (again).

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,12 @@
-#![feature(phase)]
-
-#[phase(plugin)] extern crate lazy_static;
 extern crate serialize;
-extern crate rustrt;
 extern crate rustbox;
 extern crate docopt;
 extern crate iota;
 
-#[cfg(not(test))] use std::any::{Any, AnyRefExt};
 #[cfg(not(test))] use std::io::stdio;
-#[cfg(not(test))] use std::rt::backtrace;
-#[cfg(not(test))] use std::sync::Mutex;
-#[cfg(not(test))] use rustrt::unwind;
 #[cfg(not(test))] use docopt::Docopt;
 #[cfg(not(test))] use iota::{Editor, Input};
-#[cfg(not(test))] use rustbox::RustBox;
+#[cfg(not(test))] use rustbox::{InitOption, RustBox};
 #[cfg(not(test))] static USAGE: &'static str = "
 Usage: iota [<filename>]
        iota --help
@@ -31,56 +23,17 @@ struct Args {
 }
 
 #[cfg(not(test))]
-lazy_static! {
-    // Place to hold backtraces until RustBox terminates.
-    static ref BACKTRACE: Mutex<Vec<u8>> = Mutex::new(Vec::new());
-}
-
-#[cfg(not(test))]
 fn main() {
-    struct RustBoxGuard;
-
-    impl Drop for RustBoxGuard {
-        fn drop(&mut self) {
-            let mut guard = BACKTRACE.lock();
-            if !guard.is_empty() {
-                drop(stdio::stderr().write(&**guard));
-                guard.truncate(0);
-            }
-        }
-    }
-
-    fn rustbox_panic(msg: &(Any + Send), file: &'static str, line: uint) {
-        if !rustbox::running() { return }
-        let msg = match msg.downcast_ref::<String>() {
-            Some(m) => m.clone(),
-            None => match msg.downcast_ref::<&str>() {
-                Some(m) => m.to_string(),
-                None => "".to_string()
-            }
-        };
-        let mut guard = BACKTRACE.lock();
-        drop(writeln!(guard, "panic at {}, line {}: {}", file, line, msg));
-        if backtrace::log_enabled() {
-            drop(backtrace::write(&mut *guard));
-        }
-    }
-
-    unsafe { unwind::register(rustbox_panic); }
-
     let args: Args = Docopt::new(USAGE)
                             .and_then(|d| d.decode())
                             .unwrap_or_else(|e| e.exit());
-    let source = if stdio::stdin_raw().isatty() {
-        Input::Filename(args.arg_filename)
+    let (source, buffer_stderr) = if stdio::stdin_raw().isatty() {
+        (Input::Filename(args.arg_filename), Some(InitOption::BufferStderr))
     } else {
-        Input::Stdin(stdio::stdin())
+        (Input::Stdin(stdio::stdin()), None)
     };
 
-    let _guard = RustBoxGuard; // This lets us capture errors on panic!
-    {
-        let rb = RustBox::init().unwrap();
-        let mut editor = Editor::new(source, &rb);
-        editor.start();
-    }
+    let rb = RustBox::init(&[buffer_stderr]).unwrap();
+    let mut editor = Editor::new(source, &rb);
+    editor.start();
 }
